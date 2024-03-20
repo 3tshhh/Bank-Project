b204498741f1527a2645739f2921058e5cb5ec14
youssefmoh2 <youssefmohammed3340@gmail.com>
2024-03-20 12:29:42 -0700

    comment

--- Bank/include/Client.h
+++ Bank/include/Client.h
@@ -3,6 +3,67 @@
 #include "Person.h"
 #include "Validation.h"
 class Client: public Person{
+/*class client
+{
+	//	atrri
+protected:
+	int id;
+	string name;
+	string password;
+	double balance;
+	//conc
+public:
+	client(int id, string name, string password, double balance) {
+		this->id = id;
+		this->name = name;
+		this->password = password;
+		this->balance = balance;
+	}
+	//setters
+	void setId(int id) {
+		this->id = id;
+	}
+	void setName(string name) {
+		this->name = name;
+		if (name.length() >= 5 && name.length() <= 20) {
+			cout << "Name is valid." << endl;
+		}
+		else {
+			cout << "Name must be between 5 and 20 characters." << endl;
+		}
+	}
+	void setPassword(string password) {
+		this->password = password;
+		if (password.length() >= 8 && password.length() <= 20) {
+			cout << "Password is valid." << endl;
+		}
+		else {
+			cout << "Password must be between 8 and 20 characters." << endl;
+		}
+	}
+	void setBalance(double balance) {
+		this->balance = balance;
+		if (balance >= 1500) {
+			cout << "balance is valid" << endl;
+		}
+		else {
+			cout << "the Balance must be more than 1500" << endl;
+		}
+	}
+	//getters
+	int getId() {
+		return id;
+	}
+	string getName() {
+		return name;
+	}
+	string getPassword() {
+		return password;
+	}
+	double getBalance() {
+		return balance;
+	}
+};\
 
 private:
     double balance;
