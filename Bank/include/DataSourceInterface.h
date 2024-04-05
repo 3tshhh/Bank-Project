5485034cadec42f47143cd9f0f21b29ec0d90533
youssefmoh2 <youssefmohammed3340@gmail.com>
2024-04-05 12:26:30 -0700

    data interface

---
+++ Bank/include/DataSourceInterface.h
@@ -0,0 +1,20 @@
+#ifndef DATASOURCEINTERFACE_H
+#define DATASOURCEINTERFACE_H
+
+
+class DataSourceInterface
+{
+public:
+ virtual void AddClient(Client) = 0;
+ virtual void AddEmployee(Employee) = 0;
+ virtual void AddAdmin(Admin) = 0;
+ virtual void getAllClients() = 0;
+ virtual void getAllEmployees() = 0;
+ virtual void getAllAdmins() = 0;
+ virtual void removeAllClients() = 0;
+ virtual void removeAllEmployees() = 0;
+ virtual void removeAllAdmins() = 0;
+
+};
+
+#endif // DATASOURCEINTERFACE_H
--- Bank/main.cpp
+++ Bank/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Client.h"
+#include "Employee.h"
+#include "Admin.h"
 #define long long ll
 
 using namespace std;
 
-main()
+ int main()
 {
-    cout << "test";
+    Client c("youusef", "youssefff", 7777, 4000);
+    c.printinfo();
+    cout << "===================" << endl;
+    Employee e("mohamed", "sarrrred", 555, 6666);
+    e.display();
+    cout << "===================" << endl;
+    Admin a("hatem", "mohammmed", 9797, 848484);
+    a.display();
 }
